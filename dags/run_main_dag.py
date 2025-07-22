44aa8d42e82ac86df331178928ca97b600bcd7bc
t.iskakov <4rfvBGT56>
2025-07-22 11:25:55 +0600

    change dag

--- dags/run_main_dag.py
+++ dags/run_main_dag.py
@@ -1,16 +1,60 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from datetime import datetime
-from scripts import main
+from airflow.utils.dates import days_ago
+from airflow.utils.trigger_rule import TriggerRule
 
+from scripts import download, transform, load
+
+from airflow.models import Variable
+from airflow.hooks.base import BaseHook
+from airflow.exceptions import AirflowFailException
+from airflow.models.xcom_arg import XComArg
+
+import logging
+import telegram
+
+default_args = {
+    "owner": "airflow",
+    "retries": 1,
+}
+
+def notify_telegram_on_failure(context):
+    try:
+        bot_token = Variable.get("telegram_bot_token")
+        chat_id = Variable.get("telegram_chat_id")
+        bot = telegram.Bot(token=bot_token)
+        dag_id = context["dag"].dag_id
+        task_id = context["task"].task_id
+        bot.send_message(chat_id=chat_id, text=f"DAG {dag_id} failed at task {task_id}")
+
+    except Exception as e:
+        logging.error(f"Telegram notification failed: {e}")
 
 with DAG(
-    dag_id="run_main_script_dag",
-    start_date=datetime(2024,1,1),
-    schedule_interval=None,
+    dag_id="weather_etl_pipline",
+    start_date=days_ago(1),
+    schedule_interval="@daily",
     catchup=False,
+    default_args=default_args,
+    on_failure_callback=notify_telegram_on_failure,
 ) as dag:
-    run_main= PythonOperator(
-        task_id="run_main_script_dag",
-        python_callable=main.run_main
+    t1 = PythonOperator(
+        task_id="download_weather",
+        python_callback=download.download_weather_date,
+        do_xcom_push=True
     )
+
+    t2 = PythonOperator(
+        dag_id="transform",
+        python_callback=transform.transform_data,
+        op_args=[XComArg(t1)],
+    )
+
+    t3 = PythonOperator(
+        dag_id="load",
+        python_callback=load.load_to_db,
+        op_args=[XComArg(t2)],
+        trigger_rule=TriggerRule.ALL_SUCCESS
+    )
+
+    t1 >> t2 >> t3
